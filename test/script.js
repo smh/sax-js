ca8a57854cd6ce12fe3e74e86c1d666475f2c3ef
Stein Martin Hustad <stein@hustad.com>
2011-07-11 12:50:17 +0200

    Emit text events for script/style content

--- test/script.js
+++ test/script.js
@@ -4,18 +4,18 @@ require(__dirname).test({
     ["opentag", {"name": "R","attributes": {}}],
     ["attribute", {name: "type",value: "text/javascript"}],
     ["opentag", {"name": "SCRIPT","attributes": {type: "text/javascript"}}],
+    ["text", 'document.write("<em>")'],
     ["closetag", "SCRIPT"],
     ["closetag", "R"]
   ]
 });
-
 require(__dirname).test({
   xml : '<r><style type="text/css">document.write("<em>")</style></r>',
   expect : [
     ["opentag", {"name": "R","attributes": {}}],
     ["attribute", {name: "type",value: "text/css"}],
     ["opentag", {"name": "STYLE","attributes": {type: "text/css"}}],
-//    ["text", 'document.write("em")'],
+    ["text", 'document.write("<em>")'],
     ["closetag", "STYLE"],
     ["closetag", "R"]
   ]
@@ -26,6 +26,7 @@ require(__dirname).test({
   expect : [
     ["opentag", {"name": "R","attributes": {}}],
     ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["text", 'x = "'],
     ["closetag", "SCRIPT"],
     ["closetag", "R"]
   ]
@@ -36,6 +37,7 @@ require(__dirname).test({
   expect : [
     ["opentag", {"name": "R","attributes": {}}],
     ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["text", "x = </scriptandthenotherstuff>"],
     ["closetag", "SCRIPT"],
     ["closetag", "R"]
   ]
@@ -51,14 +53,17 @@ require(__dirname).test({
   expect : [
     ["opentag", {"name": "R","attributes": {}}],
     ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["text", "//"],
     ["opencdata"],
     ["cdata", "\n  some stuff here\n//"],
     ["closecdata"],
     ["closetag", "SCRIPT"],
     ["opentag", {"name": "STYLE","attributes": {}}],
+    ["text", "/* "],
     ["opencdata"],
     ["cdata", " */\n  css css css\n/*"],
     ["closecdata"],
+    ["text", "*/"],
     ["closetag", "STYLE"],
     ["closetag", "R"]
   ]
@@ -69,9 +74,21 @@ require(__dirname).test({
   expect : [
     ["opentag", {"name": "R","attributes": {}}],
     ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["text", "//"],
     ["opencdata"],
     ["cdata", " don\'t close on embedded </script> tags within cdata sections"],
     ["closecdata"],
+    ["closetag", "SCRIPT"],
+    ["closetag", "R"]
+  ]
+});
+
+require(__dirname).test({
+  xml : '<r><script>//fake CDATA <![CDATA ]]></script></r>',
+  expect : [
+    ["opentag", {"name": "R","attributes": {}}],
+    ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["text", "//fake CDATA <![CDATA ]]>"],
     ["closetag", "SCRIPT"],
     ["closetag", "R"]
   ]
