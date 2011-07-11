10ba0462c5b6a3af6890e841b741372caae54829
Stein Martin Hustad <stein@hustad.com>
2011-07-11 11:41:18 +0200

    Just look for the closing tag inside script/style tags

--- test/script.js
+++ test/script.js
@@ -15,7 +15,46 @@ require(__dirname).test({
     ["opentag", {"name": "R","attributes": {}}],
     ["attribute", {name: "type",value: "text/css"}],
     ["opentag", {"name": "STYLE","attributes": {type: "text/css"}}],
+//    ["text", 'document.write("em")'],
     ["closetag", "STYLE"],
     ["closetag", "R"]
   ]
 });
+
+require(__dirname).test({
+  xml : '<r><script>x = "</script></r>',
+  expect : [
+    ["opentag", {"name": "R","attributes": {}}],
+    ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["closetag", "SCRIPT"],
+    ["closetag", "R"]
+  ]
+});
+
+require(__dirname).test({
+  xml : '<r><script>x = </scriptandthenotherstuff></script></r>',
+  expect : [
+    ["opentag", {"name": "R","attributes": {}}],
+    ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["closetag", "SCRIPT"],
+    ["closetag", "R"]
+  ]
+});
+
+require(__dirname).test({
+  xml : '<r><script>//<![CDATA[\n' +
+        '  some stuff here\n' +
+        '//]]></script>' +
+        '<style>/* <![CDATA[ */\n' +
+        '  css css css\n' +
+        '/*]]>*/</style></r>',
+  expect : [
+    ["opentag", {"name": "R","attributes": {}}],
+    ["opentag", {"name": "SCRIPT","attributes": {}}],
+    ["closetag", "SCRIPT"],
+    ["opentag", {"name": "STYLE","attributes": {}}],
+    ["closetag", "STYLE"],
+    ["closetag", "R"]
+  ]
+});
+
